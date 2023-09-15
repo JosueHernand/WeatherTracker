11c9204da2b1eb7d20d755af22833df5bd8a5431
JosueHernand <joseh1337@gmail.com>
2023-09-15 04:00:49 -0700

    fixed an issue for current day and future day

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,20 +8,46 @@ searchButton.addEventListener('click', function(event) {
     event.preventDefault();
     const cityName = cityInput.value.trim();
 
-    if(cityName) {
-        const listItem = document.createElement("li");
-        listItem.textContent = cityName;
-        historyList.appendChild(listItem);
-        cityInput.value = "";
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`)
+.then(function(response) {
+    return response.json();
+})
+.then(function(data) {
+    const currentCityElement = document.getElementById("currentCity");
+    const currentTemperatureElement = document.getElementById("currentTemperature");
+    const currentDescriptionElement = document.getElementById("currentDescription");
 
-        fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}`)
-        .then(function(response) {
-            return response.json();
-        })
-        .then(function(data) {
-            console.log(data);
-        })
-    }
+    currentCityElement.textContent = `City: ${data.name}`;
+    currentTemperatureElement.textContent = `Temperature: ${data.main.temp}°F`;
+    currentDescriptionElement.textContent = `Description: ${data.weather[0].description}`;
+})
+
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}`)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {  
+        console.log(data);     
+        const forecastContainer = document.querySelector(".futurecast");
+        forecastContainer.innerHTML = "";
+
+        for (let i = 7; i < data.list.length; i += 8) {
+            const forecastItem = data.list[i];
+            const forecastDate = new Date(forecastItem.dt * 1000);
+
+            const forecastElement = document.createElement("div");
+            forecastElement.classList.add("forecast-item");
+            forecastElement.innerHTML = `
+                <p>Date: ${forecastDate.toDateString()}</p>
+                <p>Temperature: ${forecastItem.main.temp}°F</p>
+                <p>Description: ${forecastItem.weather[0].description}</p>
+            `;
+
+            forecastContainer.appendChild(forecastElement);
+        }
+    })
+    .catch(function(error) {
+        console.error("Error fetching data: " + error);
+    });
+
 });
-
-// searchButton.addEventListener("submit", searchCity);
